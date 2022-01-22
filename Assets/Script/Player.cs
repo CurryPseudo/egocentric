9a36711d24bda2e01d17d6c359385599a7f2ce0b
currypseudo <currypseudo@gmail.com>
2022-01-22 13:50:09 +0800

    feat: move with friction and changing player up

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,16 +2,114 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+abstract class State
+{
+    internal Player player;
+    public abstract IEnumerator Main();
+    public virtual void DrawGizmos() { }
+}
+
+class Idle : State
+{
+    public override void DrawGizmos()
+    {
+        var dis = Mathf.Max(player.onGroundEpsilon, -Vector2.Dot(player.velocity, player.up));
+        Gizmos.DrawLine(player.foot, player.foot - player.up * dis);
+    }
+    public override IEnumerator Main()
+    {
+        while (true)
+        {
+            {
+                var up_offset = Vector2.Dot(player.velocity * Time.deltaTime, player.up);
+                var dis = Mathf.Max(player.onGroundEpsilon, -up_offset);
+                var Hit = Physics2D.CircleCast(player.pos, player.radius, -player.up, dis, player.groundLayer);
+                if (Hit)
+                    up_offset = Mathf.Max(up_offset, -Hit.distance);
+                var right_offset = Vector2.Dot(player.velocity * Time.deltaTime, player.right);
+                player.pos = player.pos + up_offset * player.up + right_offset * player.right;
+                if (Hit)
+                {
+                    player.up = Hit.normal;
+                    player.velocity = player.velocity - Vector2.Dot(player.velocity, player.up) * player.up;
+                }
+
+            }
+            player.velocity = player.velocity - player.up * player.gravity * Time.deltaTime;
+            {
+                var horizontal = Input.GetAxisRaw("Horizontal");
+                player.velocity = player.velocity + player.right * player.inputAcceration * Time.deltaTime * horizontal;
+            }
+            {
+                player.velocity = player.velocity - player.friction * player.velocity.normalized * Time.deltaTime;
+            }
+            if (Mathf.Abs(player.velocity.magnitude) < player.velocityEpsilon)
+            {
+                player.velocity = Vector2.zero;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
+
+class OnGround : State
+{
+    public override IEnumerator Main()
+    {
+        while (true)
+        {
+            yield return null;
+
+        }
+    }
+}
+
 public class Player : MonoBehaviour
 {
+    public Vector2 up = Vector2.up;
+    public Vector2 right => new Vector2(up.y, -up.x);
+    public Vector2 pos
+    {
+        get { return GetComponent<Rigidbody2D>().position; }
+        set { GetComponent<Rigidbody2D>().position = value; }
+    }
+    public Vector2 velocity = Vector2.zero;
+    public float gravity;
+    public float onGroundEpsilon = 0.1f;
+    public Vector2 foot => pos - up * radius;
+    public float radius => GetComponent<CircleCollider2D>().radius;
+    public new Collider2D collider => GetComponent<CircleCollider2D>();
+    public LayerMask groundLayer;
+    public float inputAcceration = 1.0f;
+    public float friction = 1.0f;
+    public float velocityEpsilon = 0.1f;
+
+    State state;
+
     // Start is called before the first frame update
     void Start()
     {
+        Switch(new Idle());
     }
+    void Switch(State newState)
+    {
+        if (state != null)
+        {
+            StopCoroutine(state.Main());
+        }
+        state = newState;
+        state.player = this;
+        StartCoroutine(state.Main());
+    }
+    void OnDrawGizmos()
+    {
+        if (state != null)
+            state.DrawGizmos();
 
+    }
     // Update is called once per frame
     void Update()
     {
-
     }
 }
