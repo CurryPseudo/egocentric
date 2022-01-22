c7c8113c303eebf1bf14d06a4de99d83c2d82321
currypseudo <currypseudo@gmail.com>
2022-01-23 04:04:30 +0800

    feat: self center UI

---
+++ Assets/Script/PowerBoard.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerBoard : MonoBehaviour
+{
+    RectTransform rectTransform;
+    public TransformCurve thisCurve;
+    public TransformCurve leaveCurve;
+    public TransformCurve textCurve;
+    Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        player = GameObject.FindObjectOfType<Player>();
+        if (player.maxSelfCenterCount == 0)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        StartCoroutine(EnableTextCurve());
+    }
+    IEnumerator EnableTextCurve()
+    {
+        yield return new WaitForSeconds(thisCurve.yCurve.keys[thisCurve.yCurve.keys.Length - 1].time);
+        textCurve.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var tmp = textCurve.GetComponent<TMPro.TextMeshProUGUI>();
+        if (player.shouldSelfCenter)
+        {
+            tmp.text = "You are not the moving one";
+        }
+        else
+        {
+            tmp.text = System.String.Format("Press Z x {0}", player.maxSelfCenterCount);
+        }
+        if (player.maxSelfCenterCount == 0 && !player.shouldSelfCenter)
+        {
+            textCurve.enabled = false;
+            thisCurve.enabled = false;
+            leaveCurve.enabled = true;
+        }
+    }
+}
